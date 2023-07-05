d0a0beec110c851a808ded8e0267d099d15b8114
Aziat <abdimalikovaziat@gmail.com>
2023-07-05 15:33:46 +0600

    'save'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Button from "./componentns/UI/Button";
 
 function App() {
   return <div className="App"></div>;
--- src/componentns/UI/Button.jsx
+++ src/componentns/UI/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Button = ({ children }) => {
-  return <div>{children}</div>;
+const Button = ({ children, width }) => {
+  return <div style={{ width }}>{children}</div>;
 };
 
 export default Button;
