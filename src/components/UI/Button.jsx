f0e654b5719665f447f9822616faa2cfa8f47e92
Sagyn <topchuev090906@gmail.com>
2023-07-07 20:04:01 +0600

    reusable Button finished

---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Button as ButtonMui, styled } from '@mui/material'
+
+export const Button = ({ children, variant, ...props }) => {
+   return (
+      <StyledButton props={props} variant={variant}>
+         {children}
+      </StyledButton>
+   )
+}
+
+const StyledButton = styled(ButtonMui)(({ variant, props }) => {
+   switch (variant) {
+      case 'contained':
+         return {
+            '&.MuiButtonBase-root': {
+               padding: props.padding,
+               backgroundColor: props.background,
+               color: props.color || 'white',
+               fontSize: props.fontSize,
+
+               boxSizing: 'border-box',
+               display: 'flex',
+               justifyContent: 'center',
+               alignItems: 'center',
+               gap: '6px',
+               border: '1px solid #CB11AB',
+               borderRadius: '4px',
+               lineHeight: 'normal',
+
+               '&:hover': {
+                  background: props.backgroundHover,
+               },
+
+               '&:active': {
+                  background: props.backgroundActive,
+               },
+            },
+         }
+      case 'outlined':
+         return {
+            '&.MuiButtonBase-root': {
+               padding: props.padding,
+               display: 'flex',
+               justifyContent: 'center',
+               alignItems: 'center',
+               border: '1px solid #CB11AB',
+               color: '#CB11AB',
+               '&:hover': {
+                  background: props.backgroundHover,
+                  color: 'white',
+               },
+               '&:active': {
+                  background: props.backgroundActive,
+                  color: 'white',
+               },
+            },
+         }
+      default:
+         return {}
+   }
+})
