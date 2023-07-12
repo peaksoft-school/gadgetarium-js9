a4fcaa38888ae3071665ed14a33aaa3cf8df0148
Tugolbay Kasymbekov <kasymbekovv1221@gmail.com>
2023-07-12 13:28:47 +0600

    Fixed bugs

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,72 +1,52 @@
 import React, { forwardRef, useState } from 'react'
-import TextField from '@mui/material/TextField'
 import {
-   FormControl,
    IconButton,
    InputAdornment,
    OutlinedInput,
    styled,
 } from '@mui/material'
 
-import { ReactComponent as DontWatch } from '../../assets/icons/iconsInput/eye-slash-fill.svg'
-import { ReactComponent } from '../../assets/icons/iconsInput/Eye-Icon-wsj93.svg'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 
-const InputUi = ({ error, color, type, register, id, placeholder }) => {
-   const [showPassword, setShowPassword] = useState(false)
+export const InputUi = forwardRef(
+   ({ error, color, type, id, placeholder }, ref) => {
+      const [showPassword, setShowPassword] = useState(false)
 
-   const handleClickShowPassword = () => setShowPassword((show) => !show)
+      const handleClickShowPassword = () => setShowPassword((show) => !show)
 
-   const handleMouseDownPassword = (event) => {
-      event.preventDefault()
-   }
+      const passwordType = showPassword ? 'text' : 'password'
 
-   return (
-      <div>
-         {type === 'password' ? (
-            <FormControl variant="outlined">
-               <InputOutlained
-                  error={error}
-                  color={color}
-                  placeholder={placeholder}
-                  id={id}
-                  {...register}
-                  type={showPassword ? 'text' : 'password'}
-                  endAdornment={
-                     <InputAdornment position="end">
-                        <IconButton
-                           aria-label="toggle password visibility"
-                           onClick={handleClickShowPassword}
-                           onMouseDown={handleMouseDownPassword}
-                           edge="end"
-                        >
-                           {showPassword ? <Watch /> : <DontWatch />}
-                        </IconButton>
-                     </InputAdornment>
-                  }
-               />
-            </FormControl>
-         ) : (
-            <Input
-               id="outlined-basic"
-               variant="outlined"
+      return (
+         <div>
+            <InputOutlained
                error={error}
                color={color}
-               type={type}
                placeholder={placeholder}
+               id={id}
+               ref={ref}
+               type={type === 'password' ? passwordType : type}
+               endAdornment={
+                  type === 'password' ? (
+                     <InputAdornment>
+                        <IconButton onClick={handleClickShowPassword}>
+                           {showPassword ? (
+                              <VisibilityIcon />
+                           ) : (
+                              <VisibilityOffIcon />
+                           )}
+                        </IconButton>
+                     </InputAdornment>
+                  ) : (
+                     ''
+                  )
+               }
             />
-         )}
-      </div>
-   )
-}
-
-export default forwardRef(InputUi)
+         </div>
+      )
+   }
+)
 
 const InputOutlained = styled(OutlinedInput)`
    width: 459px;
-`
-const Input = styled(TextField)`
-   width: 459px;
-`
-const Watch = styled(ReactComponent)`
-   width: 25px;
 `
