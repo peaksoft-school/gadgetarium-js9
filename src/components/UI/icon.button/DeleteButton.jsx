8b18ceb4c2172900d5b988cffe02486334c2f0d6
Baha <kingpro6561@gmail.com>
2023-07-17 23:23:10 +0600

    edit delete and play button

--- src/components/UI/icon.button/DeleteButton.jsx
+++ src/components/UI/icon.button/DeleteButton.jsx
@@ -1,42 +1,46 @@
 import { styled } from '@mui/material'
 import React from 'react'
+import { themes } from '../../../utils/common/styles/themes'
 
 const DeleteButton = () => {
    return (
-      <StyledButton>
-         <svg
-            width="10"
-            height="10"
-            viewBox="0 0 10 10"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-         >
-            <path
-               d="M9.15819 2.27268L6.43124 4.99991L9.15819 7.72702C9.56381 8.13279 9.56381 8.7901 9.15819 9.19587C8.95552 9.39854 8.68981 9.49995 8.42423 9.49995C8.15821 9.49995 7.89247 9.39869 7.68996 9.19587L4.96242 6.46846L2.23508 9.19585C2.03244 9.39851 1.7667 9.49993 1.50089 9.49993C1.23515 9.49993 0.969595 9.39867 0.766772 9.19585C0.361152 8.79025 0.361152 8.13292 0.766772 7.72699L3.49364 4.99989L0.766617 2.27268C0.360997 1.86706 0.360997 1.2096 0.766617 0.803983C1.17216 0.398672 1.82923 0.398672 2.23493 0.803983L4.96239 3.53122L7.68965 0.803983C8.09542 0.398672 8.75257 0.398672 9.15803 0.803983C9.56381 1.2096 9.56381 1.86706 9.15819 2.27268Z"
-               fill="#91969E"
-            />
-         </svg>
+      <StyledButton
+         themes={themes}
+         xmlns="http://www.w3.org/2000/svg"
+         width="18"
+         height="18"
+         viewBox="0 0 18 18"
+         fill="none"
+      >
+         <path
+            d="M13.1582 6.27268L10.4312 8.99991L13.1582 11.727C13.5638 12.1328 13.5638 12.7901 13.1582 13.1959C12.9555 13.3985 12.6898 13.5 12.4242 13.5C12.1582 13.5 11.8925 13.3987 11.69 13.1959L8.96242 10.4685L6.23508 13.1958C6.03244 13.3985 5.7667 13.4999 5.50089 13.4999C5.23515 13.4999 4.9696 13.3987 4.76677 13.1958C4.36115 12.7903 4.36115 12.1329 4.76677 11.727L7.49364 8.99989L4.76662 6.27268C4.361 5.86706 4.361 5.2096 4.76662 4.80398C5.17216 4.39867 5.82923 4.39867 6.23493 4.80398L8.96239 7.53122L11.6896 4.80398C12.0954 4.39867 12.7526 4.39867 13.158 4.80398C13.5638 5.2096 13.5638 5.86706 13.1582 6.27268Z"
+            fill="#91969E"
+         />
+         <rect
+            x="0.5625"
+            y="0.5625"
+            width="16.875"
+            height="16.8751"
+            rx="8.4375"
+            stroke="#91969E"
+            strokeWidth="1.125"
+         />
       </StyledButton>
    )
 }
 
 export default DeleteButton
-const StyledButton = styled('button')`
-   width: 18px;
-   height: 18px;
-   border: 1px solid #91969e;
-   border-radius: 100%;
-   &:hover {
-      border: 1px solid #cb11ab;
-   }
-   &:active {
-      border: 1px solid #cb11ab;
-   }
-   &:hover svg path {
-      fill: #cb11ab;
-   }
-
-   &:active svg path {
-      fill: #cb11ab;
-   }
-`
+const StyledButton = styled('svg')(({ themes }) => ({
+   '&:hover path': {
+      fill: themes.palette.primary.main,
+   },
+   '&:active path': {
+      fill: themes.palette.primary.main,
+   },
+   '&:hover rect': {
+      stroke: themes.palette.primary.main,
+   },
+   '&:active rect': {
+      stroke: themes.palette.primary.main,
+   },
+}))
