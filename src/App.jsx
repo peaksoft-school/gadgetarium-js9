475e99fe57ca89f523edb46c29346ded39661c7a
Baha <kingpro6561@gmail.com>
2023-07-17 23:24:56 +0600

    delete trash

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { ThemeProvider } from '@mui/material'
 import { themes } from './utils/common/styles/themes'
 import { CustomToast } from './components/UI/snackbar/CustomToast'
-import DeleteButton from './components/UI/icon.button/DeleteButton'
-import PlayButton from './components/UI/icon.button/PlayButton'
 
 function App() {
    return (
       <ThemeProvider theme={themes}>
          <h1>Hello World</h1>
-         <DeleteButton />
-         <PlayButton />
          <CustomToast />
       </ThemeProvider>
    )
