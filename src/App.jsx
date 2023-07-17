4e386f12fc5e9bc04aae92838342fb643868f7b6
Tugolbay Kasymbekov <kasymbekovv1221@gmail.com>
2023-07-17 19:38:41 +0600

    edit Calendar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { ThemeProvider } from '@mui/material'
 import { themes } from './utils/common/styles/themes'
 import { CustomToast } from './components/UI/snackbar/CustomToast'
 import { CalendarTest } from './components/calendar/CalendarTest'
-// import Calendar from './components/calendar/Calendar'
+import Calendar from './components/calendar/Calendar'
 
 function App() {
    return (
       <ThemeProvider theme={themes}>
          <h1>Hello World</h1>
-         {/* <Calendar /> */}
+         <Calendar />
          <CalendarTest />
          <CustomToast />
       </ThemeProvider>
--- src/components/calendar/CalendarTest.jsx
+++ src/components/calendar/CalendarTest.jsx
@@ -4,10 +4,12 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+
 import dayjs from 'dayjs'
 import 'dayjs/locale/ru'
 
 dayjs.locale('ru')
+
 export const CalendarTest = () => {
    return (
       <div>
