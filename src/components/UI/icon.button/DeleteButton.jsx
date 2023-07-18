18d773ad144a0c63e1d1a37410de91e578aa08f2
Baha <kingpro6561@gmail.com>
2023-07-18 13:10:33 +0600

    test

--- src/components/UI/icon.button/DeleteButton.jsx
+++ src/components/UI/icon.button/DeleteButton.jsx
@@ -1,46 +1,51 @@
 import { styled } from '@mui/material'
 import React from 'react'
-import { themes } from '../../../utils/common/styles/themes'
 
-const DeleteButton = () => {
+const BackButton = () => {
    return (
-      <StyledButton
-         themes={themes}
-         xmlns="http://www.w3.org/2000/svg"
-         width="18"
-         height="18"
-         viewBox="0 0 18 18"
-         fill="none"
-      >
-         <path
-            d="M13.1582 6.27268L10.4312 8.99991L13.1582 11.727C13.5638 12.1328 13.5638 12.7901 13.1582 13.1959C12.9555 13.3985 12.6898 13.5 12.4242 13.5C12.1582 13.5 11.8925 13.3987 11.69 13.1959L8.96242 10.4685L6.23508 13.1958C6.03244 13.3985 5.7667 13.4999 5.50089 13.4999C5.23515 13.4999 4.9696 13.3987 4.76677 13.1958C4.36115 12.7903 4.36115 12.1329 4.76677 11.727L7.49364 8.99989L4.76662 6.27268C4.361 5.86706 4.361 5.2096 4.76662 4.80398C5.17216 4.39867 5.82923 4.39867 6.23493 4.80398L8.96239 7.53122L11.6896 4.80398C12.0954 4.39867 12.7526 4.39867 13.158 4.80398C13.5638 5.2096 13.5638 5.86706 13.1582 6.27268Z"
-            fill="#91969E"
-         />
-         <rect
-            x="0.5625"
-            y="0.5625"
-            width="16.875"
-            height="16.8751"
-            rx="8.4375"
-            stroke="#91969E"
-            strokeWidth="1.125"
-         />
+      <StyledButton>
+         <svg
+            width="15"
+            height="13"
+            viewBox="0 0 15 13"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+         >
+            <path
+               d="M14 6.5H1M1 6.5L6.42466 1M1 6.5L6.42466 12"
+               stroke="#CB11AB"
+               strokeWidth="2"
+               strokeLinecap="round"
+               strokeLinejoin="round"
+            />
+         </svg>
       </StyledButton>
    )
 }
 
-export default DeleteButton
-const StyledButton = styled('svg')(({ themes }) => ({
-   '&:hover path': {
-      fill: themes.palette.primary.main,
-   },
-   '&:active path': {
-      fill: themes.palette.primary.main,
-   },
-   '&:hover rect': {
-      stroke: themes.palette.primary.main,
-   },
-   '&:active rect': {
-      stroke: themes.palette.primary.main,
-   },
-}))
+export default BackButton
+const StyledButton = styled('button')`
+   width: 50px;
+   height: 50px;
+   border-radius: 100%;
+   border: none;
+   background-color: white;
+   transition: background-color 0.3s;
+   &:hover {
+      background-color: #cb11ab;
+   }
+   &:active {
+      background-color: #cb11ab;
+   }
+   svg path {
+      transition: stroke 0.3s;
+   }
+
+   &:hover svg path {
+      stroke: white;
+   }
+
+   &:active svg path {
+      stroke: white;
+   }
+`
