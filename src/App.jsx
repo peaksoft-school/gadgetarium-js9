fc1533ea79141ecbe4a9f0c6c91ae489e5a54758
Baha <kingpro6561@gmail.com>
2023-07-18 15:18:45 +0600

    delete component in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import { ThemeProvider } from '@mui/material'
 import { themes } from './utils/common/styles/themes'
 import { CustomToast } from './components/UI/snackbar/CustomToast'
-import ForthButton from './components/UI/icon.button/back.forth.buttons/ForthButton'
-import PlayButton from './components/UI/icon.button/PlayButton'
-import DeleteButton from './components/UI/icon.button/DeleteButton'
-import BackButton from './components/UI/icon.button/back.forth.buttons/BackButton'
 
 function App() {
    return (
       <ThemeProvider theme={themes}>
          <h1>Hello World</h1>
          <CustomToast />
-         <ForthButton bigButton />
-         <PlayButton />
-         <DeleteButton />
-         <BackButton bigButton />
       </ThemeProvider>
    )
 }
