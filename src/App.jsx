4b7932be4fb27ad7140ef3c6e8ce2a1222bb60c1
Daniel <danielkyrgyz200715@gmail.com>
2023-07-18 10:07:41 +0600

    edit app.js and pull development

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { ThemeProvider } from '@mui/material'
+import { BrowserRouter } from 'react-router-dom'
 import { themes } from './utils/common/styles/themes'
 import { CustomToast } from './components/UI/snackbar/CustomToast'
-import { BrowserRouter } from 'react-router-dom'
 
 function App() {
    return (
       <BrowserRouter>
-        <ThemeProvider theme={themes}>
-         <h1>Hello World</h1>
-         <CustomToast />
-      </ThemeProvider>
+         <ThemeProvider theme={themes}>
+            <h1>Hello World</h1>
+
+            <CustomToast />
+         </ThemeProvider>
       </BrowserRouter>
-    
    )
 }
 
