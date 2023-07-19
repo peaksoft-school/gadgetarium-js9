29e8202cd361ed474b0e04e68a1ed036922d433f
Baha <kingpro6561@gmail.com>
2023-07-19 10:55:12 +0600

    add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from '@mui/material'
 import { themes } from './utils/common/styles/themes'
 import { CustomToast } from './components/UI/snackbar/CustomToast'
+import BasicTable from './components/UI/Table'
 
 function App() {
    return (
       <ThemeProvider theme={themes}>
          <h1>Hello World</h1>
          <CustomToast />
+         <BasicTable />
       </ThemeProvider>
    )
 }
