cbbdb850798bba0e98231e36981f72129846c503
Baha <kingpro6561@gmail.com>
2023-07-22 16:52:23 +0600

    delete in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { ThemeProvider } from '@mui/material'
 import { themes } from './utils/common/styles/themes'
 import { CustomToast } from './components/UI/snackbar/CustomToast'
-import AdminTable from './components/UI/admin.table/Table'
 
 function App() {
    return (
       <ThemeProvider theme={themes}>
          <h1>Hello World</h1>
          <CustomToast />
-         <AdminTable index={1} itemTableArray={[]} />
       </ThemeProvider>
    )
 }
