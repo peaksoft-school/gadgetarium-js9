792e0b3e5faedd9aefc861d9159c6239d617903e
Tugolbay Kasymbekov <kasymbekovv1221@gmail.com>
2023-07-23 23:09:54 +0600

    edit SignIn and SignUp Form

--- src/components/UI/icon.input/CheckboxInput.jsx
+++ src/components/UI/icon.input/CheckboxInput.jsx
@@ -1,20 +1,17 @@
+import React, { useState } from 'react'
 import React from 'react'
 import Checkbox from '@mui/material/Checkbox'
 import { styled } from '@mui/material'
 
+function CheckboxInput({isChecked, onChange}) {
 function CheckboxInput({ isChecked, onChange }) {
    return (
+      <StyledCheckbox
+         checked={isChecked}
+         onChange={onChange}
+         color="primary"
+      />
       <StyledCheckbox checked={isChecked} onChange={onChange} color="primary" />
    )
 }
 
-export default CheckboxInput
-const StyledCheckbox = styled(Checkbox)`
-   color: gray;
-   &:hover {
-      color: #cb11ab;
-   }
-   &.Mui-checked {
-      color: #cb11ab;
-   }
-`
