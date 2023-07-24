5f735a0beb72ce05f7640d8116b090811de1862d
Baha <kingpro6561@gmail.com>
2023-07-24 12:07:26 +0600

    adding object

--- src/components/UI/admin.table/Table.jsx
+++ src/components/UI/admin.table/Table.jsx
@@ -113,3 +113,66 @@ const StyledTableCell = styled(TableCell)`
    letter-spacing: 0.0625rem;
    padding: 0;
 `
+
+/*
+Table 0
+
+{
+  id: '1',
+  photo:
+     'https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png',
+  vendorCode: '14134242',
+  modelName: 'Samsung S21 UltraNanoBich',
+  purchaseTime: '07:21:2023 12:20',
+  quantity: 10,
+  productPrice: 50000,
+  discount: 10,
+}
+*/
+
+/* 
+Table 1
+
+{
+  id: '1',
+  photo:
+     'https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png',
+  modelName: 'Samsung S21 UltraNanoBich',
+  color: 'Красный',
+  quantityOfSIMCart: '1',
+  RAM: '6',
+  ROM: '64',
+  quantity: 10,
+  productPrice: 50000,
+}
+*/
+
+/*
+Table 2
+
+{
+  id: '1',
+  fullName: 'Баха Джаркимбаев',
+  number: '000000-455247',
+  quantity: 10,
+  totalSum: '75000',
+  ordering: 'Самовывоз',
+  status: 'В обработке',
+},
+*/
+
+/*
+Table 3
+
+{
+  id: '1',
+  brand: 'Apple',
+  color: 'Красный',
+  memory: '32',
+  RAM: '6',
+  quantityOfSIMCart: '1',
+  purchaseTime: '07:21:2023 12:20',
+  quantityOfGoods: '24',
+  productPrice: 50000,
+},
+*/
