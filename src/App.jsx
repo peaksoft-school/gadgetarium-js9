6a1fa989fa3d7636addbcd556f6f2594c55c849e
Baha <kingpro6561@gmail.com>
2023-08-11 16:17:40 +0600

    deleting mailingist

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { MailingList } from './components/admin/mailing.list/MailingList'
 import { UserLayout } from './layout/user/UserLayout'
 
 function App() {
    return (
       <div>
          <UserLayout />
-         <MailingList />
       </div>
    )
 }
