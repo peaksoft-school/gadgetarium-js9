b61cf5538269807e1e1ccf6b9005e66af93c5a8a
Daniel <danielkyrgyz200715@gmail.com>
2023-09-05 11:36:42 +0600

    {

--- src/components/admin/addingAProduct/partThree/inputAddProductPartThree/InputDescription.jsx
+++ src/components/admin/addingAProduct/partThree/inputAddProductPartThree/InputDescription.jsx
@@ -1,121 +1,130 @@
-import React, { useEffect, useState } from 'react'
-import { Editor, EditorState, RichUtils } from 'draft-js'
-import 'draft-js/dist/Draft.css'
-import { IconButton, styled } from '@mui/material'
+import React from 'react'
+import { EditorProvider, useCurrentEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import { styled, IconButton } from '@mui/material'
+import { BulletList } from '@tiptap/extension-bullet-list'
+import { OrderedList } from '@tiptap/extension-ordered-list'
+import Underline from '@tiptap/extension-underline'
 import FormatBoldIcon from '@mui/icons-material/FormatBold'
 import FormatItalicIcon from '@mui/icons-material/FormatItalic'
 import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined'
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted'
 import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered'
-// import { ReactComponent as BoldIcon } from '../../../../.
-// ./assets/icons/editor-text-tools/bold-icon.svg'
-// import { ReactComponent as ItalicIcon } from '../../../..
-// /../assets/icons/editor-text-tools/italic-icon.svg'
-// import { ReactComponent as OrderedListIcon } from '../../
-// ../../../assets/icons/editor-text-tools/ordered-list-icon.svg'
-// import { ReactComponent as UnderLineIcon } from '../../..
-// /../../assets/icons/editor-text-tools/under-line-icon.svg'
-// import { ReactComponent as UnorderedListIcon } from '../../..
-// /../../assets/icons/editor-text-tools/unordered-list-icon.svg'
 
-export const InputDescription = () => {
-   const [editorState, setEditorState] = useState(EditorState.createEmpty())
-   const [rr, setRr] = useState(false)
-   console.log('editorState: ', editorState)
-   console.log('rr: ', rr)
+export function MenuBar() {
+   const { editor } = useCurrentEditor()
 
-   function onChange(editorState) {
-      setEditorState(editorState)
-   }
-
-   const handleKeyCommand = (command) => {
-      console.log('command: ', command)
-      const newState = RichUtils.handleKeyCommand(editorState, command)
-
-      if (newState) {
-         setEditorState(newState)
-         return 'handled'
-      }
-
-      // if (rr) {
-      //    setEditorState(RichUtils.toggleInlineStyle(editorState, 'ITALIC'))
-      // }
-
-      return 'not-handled'
-   }
-
-   useEffect(() => {
-      if (rr) {
-         setEditorState(RichUtils.toggleBlockType(editorState, 'BOLD'))
-      }
-   }, [rr])
-
-   const onBoldClick = () => {
-      setRr((prev) => !prev)
+   if (!editor) {
+      return null
    }
 
    return (
-      <Container>
-         <ContainerButton>
-            <IconButton onClick={onBoldClick}>
-               <FormatBoldIcon />
-            </IconButton>
-            <IconButton onClick={onBoldClick}>
-               <FormatItalicIcon />
-            </IconButton>
-            <IconButton onClick={onBoldClick}>
-               <FormatUnderlinedIcon />
-            </IconButton>
-            <IconButton onClick={onBoldClick}>
-               <FormatListBulletedIcon />
-            </IconButton>
-            <IconButton onClick={onBoldClick}>
-               <FormatListNumberedIcon />
-            </IconButton>
-         </ContainerButton>
+      <ContainerButton>
+         <IconButton
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            disabled={!editor.can().chain().focus().toggleBold().run()}
+            className={editor.isActive('bold') ? 'is-active' : ''}
+         >
+            <FormatBoldIcon />
+         </IconButton>
+         <IconButton
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            disabled={!editor.can().chain().focus().toggleItalic().run()}
+            className={editor.isActive('italic') ? 'is-active' : ''}
+         >
+            <FormatItalicIcon />
+         </IconButton>
+         <IconButton
+            onClick={() => editor.chain().focus().toggleUnderline().run()}
+            disabled={!editor.can().chain().focus().toggleUnderline().run()}
+            className={editor.isActive('underline') ? 'is-active' : ''}
+         >
+            <FormatUnderlinedIcon />
+         </IconButton>
+         <IconButton
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={editor.isActive('bulletList') ? 'is-active' : ''}
+         >
+            <FormatListBulletedIcon />
+         </IconButton>
+         <IconButton
+            onClick={() => editor.chain().focus().toggleOrderedList().run()}
+            className={editor.isActive('orderedList') ? 'is-active' : ''}
+         >
+            <FormatListNumberedIcon />
+         </IconButton>
+      </ContainerButton>
+   )
+}
 
-         <ContainerInput>
-            <EditorStyle
-               editorState={editorState}
-               handleKeyCommand={handleKeyCommand}
-               onChange={onChange}
-               textAlignment="right"
-            />
-         </ContainerInput>
+const content = '<div></div>'
+
+const extensions = [StarterKit, Underline, BulletList, OrderedList]
+
+export const InputDescription = () => {
+   return (
+      <Container>
+         <EditorProvider
+            slotBefore={<MenuBar />}
+            extensions={extensions}
+            content={content}
+         />
       </Container>
    )
 }
 
 const Container = styled('div')`
-   border: 1px solid #000;
-   width: 694px;
+   width: 49.4rem;
+   height: 22.1875rem;
+   margin: 0;
+   border: 1px solid #cdcdcd;
+   border-radius: 4px;
+
+   .ProseMirror {
+      min-height: 17.9375rem;
+      max-height: 17.9375rem;
+      border: none;
+      outline: none;
+      padding: 1rem 1.25rem;
+      overflow-y: auto;
+
+      ul {
+         list-style: disc;
+         padding-left: 1rem;
+      }
+
+      ol {
+         list-style-type: decimal;
+         padding-left: 1rem;
+      }
+
+      p {
+         margin: 0;
+      }
+   }
 `
 
 const ContainerButton = styled('div')`
    padding: 1.25rem 3.1875rem;
    display: flex;
-   align-items: center;
    gap: 5rem;
+   border-bottom: 1px solid #cdcdcd;
 
    button {
       margin: 0;
-      border: 1px solid #ff73df;
       border-radius: 1px;
       padding: 0px;
-      /* background-color: #ffbee8; */
+   }
+
+   .is-active {
+      svg {
+         fill: #f011cb;
+      }
    }
 
    svg {
-      fill: red;
-      height: 28px;
-      width: 28px;
+      fill: #292929;
+      height: 1.75rem;
+      width: 1.75rem;
    }
 `
-
-const ContainerInput = styled('div')`
-   border: 1px solid #000;
-   width: 694px;
-   height: 287px;
-`
-
-const EditorStyle = styled(Editor)``
